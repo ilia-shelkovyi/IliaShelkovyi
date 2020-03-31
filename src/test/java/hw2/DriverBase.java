731e9f513b41ebe878fcca79c95a37b18669d02b
ilia-shelkovyi <doctorbatcoh@gmail.com>
2020-03-31 16:52:35 +0300

    Fix .gitignore

--- src/test/java/hw2/DriverBase.java
+++ src/test/java/hw2/DriverBase.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,8 +17,10 @@ public class DriverBase {
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+        /*WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();*/
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
     }
