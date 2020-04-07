b4be0330f6057eea4f95a6a40a47c08db3670ed1
ilia-shelkovyi <doctorbatcoh@gmail.com>
2020-04-07 21:57:41 +0300

    Add Exercise 3 + Fix code duplication

--- src/main/java/hw4/composite/MetalColorsContent.java
+++ src/main/java/hw4/composite/MetalColorsContent.java
@@ -29,46 +29,57 @@ public class MetalColorsContent extends AbstractPage {
     @FindBy(id = "submit-button")
     private WebElement submitButton;
 
+    @FindBy(xpath = "//*[@class='checkbox']/*[text()='Vegetables']")
+    private WebElement vegetablesCheckbox;
+
     public MetalColorsContent(WebDriver driver) {
         super(driver);
     }
 
     public void selectSummary(List<String> values) {
     	if(values != null) {
-    		summary.stream().filter(s -> values.contains(s.getText())).forEach(WebElement::click);
+    		selectByList(values, summary);
     	}
     }
 
     public void selectElements(List<String> values) {
     	if(values != null) {
-    		elements.stream().filter(s -> values.contains(s.getText())).forEach(WebElement::click);
+    		selectByList(values, elements);
     	}
     }
 
     public void selectColor(String value) {
     	if(value != null) {
 	    	colorsButton.click();
-	    	dropdown.stream().filter(s -> s.getText().equals(value)).forEach(WebElement::click);
+	    	selectByString(value);
     	}
     }
 
     public void selectMetal(String value) {
     	if(value != null) {
 	    	metalsButton.click();
-	    	dropdown.stream().filter(s -> s.getText().equals(value)).forEach(WebElement::click);
+	    	selectByString(value);
     	}
     }
 
     public void selectVegetables(List<String> values) {
 		vegetablesButton.click();
-		dropdown.stream().filter(s -> s.getText().equals("Vegetables")).forEach(WebElement::click);
+		vegetablesCheckbox.click();
         if(values != null) {
-    		dropdown.stream().filter(s -> values.contains(s.getText())).forEach(WebElement::click);
+        	selectByList(values, dropdown);
     	}
     }
 
     public void submit() {
     	submitButton.click();
+    }
+
+    private void selectByList(List<String> values, List<WebElement> elements) {
+    	elements.stream().filter(s -> values.contains(s.getText())).forEach(WebElement::click);
+    }
+
+    private void selectByString(String value) {
+	    dropdown.stream().filter(s -> s.getText().equals(value)).forEach(WebElement::click);
     }
 
 }
