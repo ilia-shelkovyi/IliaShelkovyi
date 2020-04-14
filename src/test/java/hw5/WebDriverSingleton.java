7722459ee3d78c142b860904dd5ce86df00d4fa4
ilia-shelkovyi <62811677+ilia-shelkovyi@users.noreply.github.com>
2020-04-14 16:04:34 +0300

    Update WebDriverSingleton.java

--- src/test/java/hw5/WebDriverSingleton.java
+++ src/test/java/hw5/WebDriverSingleton.java
@@ -2,7 +2,6 @@ package hw5;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
